df0ec06a9ea9f56ed9ecf3da97ddba8e64ea65c9
Satvik-M <76990760+Satvik-M@users.noreply.github.com>
2021-07-02 12:43:58 +0530

    fixed the exposed sendgrid key

--- mail.js
+++ mail.js
@@ -6,7 +6,7 @@ const client = nodemailer.createTransport({
   secure: false,
   auth: {
     user: "apikey",
-    pass: "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",
+    pass: sendgrid_key,
   },
 });
 const email = (module.exports.authMail = (mail_id, verify) =>
